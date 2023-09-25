6c083f0181a246a0c0af783892dcedc50847c3fa
Kostya <kostikbushmelev@gmail.com>
2023-09-26 00:28:59 +0300

    add Collection

--- src/main/java/controller/AreaCheckServlet.java
+++ src/main/java/controller/AreaCheckServlet.java
@@ -10,11 +10,40 @@ import java.io.IOException;
 public class AreaCheckServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		run(request, response);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		run(request, response);
+	}
 	
+	public void run(HttpServletRequest request, HttpServletResponse response) {
+		double x = Double.parseDouble(request.getParameter("x"));
+		double y = Double.parseDouble(request.getParameter("y"));
+		double R = Double.parseDouble(request.getParameter("R"));
+		
+		boolean res = checkPlace(x, y, R);
+		
+	}
+	
+	public boolean checkPlace(double x, double y, double R) {
+		boolean res = false;
+		if (x >= 0 && y >= 0) res = triangle(x, y, R);
+		else if (x <= 0 && y >= 0) res = circle(x, y, R);
+		else if (x <= 0 && y <= 0) res = square(x, y, R);
+		return res;
+	}
+	
+	private boolean triangle(double x, double y, double R) {
+		return (y <= -x + R / 2);
+	}
+	
+	private boolean circle(double x, double y, double R) {
+		return (y <= Math.sqrt(Math.pow(R, 2) - Math.pow(x, 2)));
+	}
+	
+	private boolean square(double x, double y, double R) {
+		return (x >= -R && y >= -R);
 	}
 }
---
+++ src/main/java/model/DotesCollection.java
@@ -0,0 +1,8 @@
+package model;
+import jakarta.ejb.Stateful;
+import jakarta.enterprise.context.SessionScoped;
+
+@Stateful
+
+public class DotesCollection {
+}
