56c5de9ab1ffa203cda7b0efebad9eef8859b110
Kostya <kostikbushmelev@gmail.com>
2023-09-26 00:53:44 +0300

    add Dote class

---
+++ src/main/java/model/Dote.java
@@ -0,0 +1,3 @@
+package model;
+
+record Dote(double x, double y) {}
--- src/main/java/model/DotesCollection.java
+++ src/main/java/model/DotesCollection.java
@@ -2,7 +2,25 @@ package model;
 import jakarta.ejb.Stateful;
 import jakarta.enterprise.context.SessionScoped;
 
-@Stateful
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class DotesCollection {
+@Stateful
+@SessionScoped
+public class DotesCollection implements Serializable {
+	private ArrayList<Dote> collection;
+	
+	public DotesCollection() {
+		create();
+	}
+	private void create() {
+		if (collection == null) this.collection = new ArrayList<>();
+	}
+	
+	public void add (Dote dote) {
+		create();
+		collection.add(dote);
+	}
+	
+	public ArrayList<Dote> get () {return collection;}
 }
